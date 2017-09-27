5f8b4c802e7b86a6946eeca841f9a2343f6414c4
Joseph Bourne <joebourne2010@gmail.com>
2017-09-26 21:12:26 -0400

    added low pass filter and incorporated virtual temperature density calculation for release rate and wind velocity

--- src/dwyer_sensor.cpp
+++ src/dwyer_sensor.cpp
@@ -11,6 +11,7 @@
 #include <wiringPi.h>
 
 #include "ros/ros.h"
+#include "lowPass.h"
 #include <std_msgs/Header.h>
 #include <sensor_msgs/FluidPressure.h>
 
@@ -21,12 +22,16 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "dwyer_sensor");
   ros::NodeHandle n;
   ros::Publisher pub = n.advertise<sensor_msgs::FluidPressure>("dwyer_pressure", 1);
-
+  ros::Publisher filPub = n.advertise<sensor_msgs::FluidPressure>("dwyer_pressure_filtered", 1);
 
   FILE *fd = NULL;  
   
   int lr;
   n.getParam("/loopR", lr);
+
+  double tc; // time constant;
+  n.getParam("/tc", tc);
+  
   
   ros::Rate loop_rate(lr);
   int adcValue;
@@ -36,6 +41,8 @@ int main(int argc, char **argv)
   int biasCheck;
   n.getParam("/biasC", biasCheck);
   double bias = 0;
+  
+  LOWPASS lp(tc);
   
   //std::cout<<fd<<std::endl;
   int c=0;
@@ -51,13 +58,20 @@ int main(int argc, char **argv)
       i = atof(buf)*bias; // bias is average value of pressure during beginning of startup
       p = (i*.03125-.125)*248.84; // (Pa)
       //std::cout<<"adc: "<<atof(buf)<<" units"<<"   i: "<<i<<" mA"<<"   p: "<<p<<" Pa"<<std::endl;
+
       
       sensor_msgs::FluidPressure msg;
       msg.header.stamp =ros::Time::now();
       msg.header.frame_id ="source";    
-      msg.fluid_pressure = p;
-      
+      msg.fluid_pressure = p;      
       pub.publish(msg);
+
+      sensor_msgs::FluidPressure msgF;
+      msgF.header.stamp =ros::Time::now();
+      msgF.header.frame_id ="source";    
+      msgF.fluid_pressure = lp.filter(msg.fluid_pressure);
+      filPub.publish(msgF);
+      
     }
     else{      
       bias+=atof(buf);
--- src/release_vel_node.cpp
+++ src/release_vel_node.cpp
@@ -3,7 +3,6 @@
 #include <time.h>
 #include <math.h>
 
-
 #include "ros/ros.h"
 #include <std_msgs/Header.h>
 #include <sensor_msgs/FluidPressure.h>
@@ -12,16 +11,25 @@
 
 
 double p;
+double tT=0;
+double bT=0;
 double T=0;
-double rh;
+double trh=0;
+double brh=0;
+double rh=0;
 
 void pressureCallback(const sensor_msgs::FluidPressure &msg){
   p = msg.fluid_pressure;
 }
 
-void wbCallback(const weather_board_driver::wb_data msg){
-  T = msg.temperature+273.15;  // Kelvin
-  rh = msg.humidity;
+void topwbCallback(const weather_board_driver::wb_data msg){
+  tT  = msg.temperature+273.15;  // Kelvin
+  trh = msg.humidity;
+}
+
+void botwbCallback(const weather_board_driver::wb_data msg){
+  bT  = msg.temperature+273.15;  // Kelvin
+  brh = msg.humidity;
 }
 
 int main(int argc, char **argv)
@@ -30,8 +38,9 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "release_vel_node");
   ros::NodeHandle n;
   ros::Publisher pub = n.advertise<dwyer_sensor_driver::releaseVel>("release_vel", 1);
-  ros::Subscriber subDwyer = n.subscribe("dwyer_pressure", 1, pressureCallback);
-  ros::Subscriber subWB = n.subscribe("wb_data", 1, wbCallback);
+  ros::Subscriber subDwyer = n.subscribe("dwyer_pressure_filtered", 1, pressureCallback);
+  ros::Subscriber topsubWB = n.subscribe("top/wb_sensor/wb_data", 1, topwbCallback);
+  ros::Subscriber botsubWB = n.subscribe("bot/wb_sensor/wb_data", 1, botwbCallback);
   
   int lr;
   n.getParam("/loopR", lr);
@@ -41,31 +50,51 @@ int main(int argc, char **argv)
   n.getParam("/radi", r);
   r = r*0.0254; //3.*0.0254; // radi of outlet
   double A   = M_PI*pow(r,2.);// area of outlet
-  double rho = 1.225;    // kg/m^3  
-  double R_specific = 287.058; //461.495; // J/kg*K
+  double R_specific = 287.3; // J/kg*K
 
-  //std::cout<<A<<std::endl;
-  ROS_INFO("[relvel] radius of outlet: %f (m)", r);
-  ROS_INFO("[relvel] Area of outlet: %f (m^2)", A);
+  // virtual temperature vars //
+  double rho = 1.225; // kg/m^3  
+  double q   = 0;     // specific humidity
+  double Tv  = 0;     // virtual temperature
+  double e   = 0;     // vapor pressure
+  double es  = 0;     // saturated vapor pressure
+  double es0 = 611;   // (Pa) reference saturation vapor pressure
+  double T0  = 273.15;// reference temperature
+  double lv  = (2.5*pow(10.0,6.0)); // latent heat of vaporization of water
+  double Rv  = 461.5; // gas constant for water vapor
   
+  ROS_INFO("[relvel] radius of outlet: %f (m)", r);
+  ROS_INFO("[relvel] Area of outlet: %f (m^2)", A);  
   ros::Rate loop_rate(lr);
   
   while (ros::ok()) {
 
-    if (p>0){
-      if (T==0){T=294.15;}       
-      rho = (p+101325)/(R_specific*T); // kg/m^3
-      //ROS_INFO("[relvel] p: %f", (p+101325));
-      //ROS_INFO("[relvel] rho: %f", rho);
-      dwyer_sensor_driver::releaseVel msg;    
-      msg.header.stamp =ros::Time::now();
-      msg.header.frame_id ="source";
-      msg.windRate = sqrt(2*p/(rho*(1+K_l)));
-      msg.releaseRate = msg.windRate*A*rho;
-      
-      pub.publish(msg);
+    if (p<0){
+      p = 0;
+      ROS_INFO_ONCE("Pressure is negative, check power source for dwyer");
     }
-	
+    
+    //if (tT==0){T=294.15;}
+    T   = (tT+bT)/2.0;
+    rh  = (trh + brh)/2.0;
+    //ROS_INFO("T: %f", T);
+    //ROS_INFO("rh: %f", rh);
+    
+    es  = es0*exp(lv/Rv*(1/T0-1/T));
+    e   = rh/100.0*es;
+    q   = 0.622*(e/(p+101325));
+    Tv  = T*(1.0+0.61*q);
+    rho = (p+101325)/(R_specific*Tv); // kg/m^3
+    //ROS_INFO("lv, Rv, es, e, q, Tv, rho: %f, %f %f, %f, %f, %f, %f", lv, Rv, es,e,q,Tv,rho);
+    
+    dwyer_sensor_driver::releaseVel msg;    
+    msg.header.stamp =ros::Time::now();
+    msg.header.frame_id ="source";
+    msg.windRate = sqrt(2*p/(rho*(1+K_l)));
+    msg.releaseRate = msg.windRate*A*rho;
+    
+    pub.publish(msg);
+    
     ros::spinOnce();
     loop_rate.sleep();
   }
